2f47a8ccc2489518d383f382e7aab60a6fcff4d7
sandeepdharma <spandala@msystechnologies.com>
2024-01-30 12:58:25 +0530

    comment changes

--- web/src/components/header/header.js
+++ web/src/components/header/header.js
@@ -8,7 +8,7 @@ export const Header = ({ prjTitle }) => {
   return (
     <Row className="header-container">
       <div className="logo-container">
-        <img src="/images/msys-group.svg" alt="msys-logo" />
+        <img src="/images/msys-group.svg" alt={i18n.t("common.logo")} />
         <p className="csat">
           {i18n.t("header.title")} <span>{i18n.t("header.proto")}</span>
         </p>
