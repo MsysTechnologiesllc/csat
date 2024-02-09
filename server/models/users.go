dd0f6279ff7e76db0bd898b3bd11bd411b118e35
Rahul Sharma <128804458+rahulrs-msys@users.noreply.github.com>
2024-02-09 14:03:38 +0530

    user Projects Details (#29)
    Signed-off-by: raj-msys <101859696+raj-msys@users.noreply.github.com>
    Co-authored-by: raj-msys <101859696+raj-msys@users.noreply.github.com>

--- server/models/users.go
+++ server/models/users.go
@@ -374,6 +374,24 @@ func GetUsersByProjectID(projectID uint) ([]schema.User, error) {
 	return users, nil
 }
 
+func GetUserProjectsDetailsByID(userID uint) map[string]interface{} {
+	var user schema.User
+
+    if err := GetDB().Preload("Projects").Where("id = ?", userID).First(&user).Error; err != nil {
+        logger.Log.Println("Error", err)
+        return u.Message(false, constants.USER_NOT_FOUND)
+	}
+
+    // Extract project details
+    var projects []schema.Project
+	projects = user.Projects
+
+    response := map[string]interface{}{
+        "projects": projects, // Include project details in the response
+    }
+	return response
+}
+
 func GetUsersListByProjectID(projectID uint) ([]schema.User, error) {
 	var users []schema.User
 
