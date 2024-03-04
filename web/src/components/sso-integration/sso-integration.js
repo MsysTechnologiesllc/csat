6704d0f8de458edb05bce12f22dec54d3a10e05a
devanandini-msys <dbunga@msystechnologies.com>
2024-03-04 12:27:11 +0530

    login api integration

--- web/src/components/sso-integration/sso-integration.js
+++ web/src/components/sso-integration/sso-integration.js
@@ -67,7 +67,17 @@ export const SSOIntegration = () => {
     }
   };
   const handleFinishAdmin = (values) => {
-    console.log(values);
+    const payload = {
+      email: values.username,
+      password: values.password,
+    };
+    new PostService().postLoginDetails(payload, (result) => {
+      if (result?.status === 200) {
+        console.log(result?.data);
+        Cookies.set("jwt", result?.data?.user?.token);
+        navigate("/accounts");
+      }
+    });
   };
   const items = [
     {
