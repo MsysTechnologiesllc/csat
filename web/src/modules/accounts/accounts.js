bea7dc0ab7ce9c21a086a2be655d73ef3436817e
devanandini-msys <dbunga@msystechnologies.com>
2024-03-05 11:28:45 +0530

    made changes in accounts.js

--- web/src/modules/accounts/accounts.js
+++ web/src/modules/accounts/accounts.js
@@ -46,27 +46,30 @@ export const Accounts = () => {
         )
       ) : (
         <Row gutter={[20, 20]} className="project-list-wrapper">
-          {accountsList.map((account) => {
+          {accountsList?.map((account) => {
             const deliveryHead =
-              account.account_projects[0].Users.length > 0
-                ? account.account_projects[0].Users
-                : account.account_projects[1].Users.filter(
-                    (each) => each.role === "deliveryHead",
-                  );
+              account?.account_projects?.length > 0 &&
+              account?.account_projects[0]?.Users?.length > 0
+                ? account?.account_projects[0]?.Users
+                : account?.account_projects[1]?.Users?.filter((each) => {
+                    each?.role === "deliveryHead";
+                  });
             return (
-              <Col xs={24} md={12} lg={8} xxl={6} key={account.ID}>
+              <Col xs={24} md={12} lg={8} xxl={6} key={account?.ID}>
                 <Card className="project-wrapper">
                   <div className="project-client-context-day-container">
                     <div className="avatar-project-client-context-container">
                       <p className="avatar">
-                        {`${account.name
+                        {`${account?.name
                           .split(" ")
-                          .map((word) => word.charAt(0).toUpperCase())
+                          .map((word) => word?.charAt(0)?.toUpperCase())
                           .join("")}`}
                       </p>
                       <div className="project-client-container">
-                        <h4 className="project-name">{account.name}</h4>
-                        <p className="client-name">{deliveryHead[0]?.name}</p>
+                        <h4 className="project-name">{account?.name}</h4>
+                        <p className="client-name">
+                          {deliveryHead?.length > 0 && deliveryHead[0]?.name}
+                        </p>
                       </div>
                     </div>
                   </div>
