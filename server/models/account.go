83938e7caaeac636e81261f4a3f5e88eb6607ea3
rahulrs-msys <sharmar@msystechnologies.com>
2024-03-07 13:06:24 +0530

    added acitve field

--- server/models/account.go
+++ server/models/account.go
@@ -15,7 +15,9 @@ func GetAccountDetails(tenantID uint) map[string]interface{} {
     // Fetch the tenant details with the given tenantID
     if err := GetDB().Preload("Accounts", func(db *gorm.DB) *gorm.DB {
 		return db.Where("is_active = ?", true)
-	}).Preload("Accounts.Projects").Preload("Accounts.Projects.Users").Where("id = ?", tenantID).First(&tenant).Error; err != nil {
+	}).Preload("Accounts.Projects", func(db *gorm.DB) *gorm.DB {
+        return db.Where("active = ?", true)
+    }).Preload("Accounts.Projects.Users").Where("id = ?", tenantID).First(&tenant).Error; err != nil {
         logger.Log.Println("Error", err)
         return u.Message(false, constants.TENANT_NOT_FOUND)
     }
