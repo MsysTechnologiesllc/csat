ab1c8170410fdb115436422be707be0ef5998c23
silambu-msys <107907502+silambu-msys@users.noreply.github.com>
2024-03-25 18:32:14 +0530

    Restriction based on user role (#95)
    * Create LICENSE
    
    Signed-off-by: Dominic <dominic097@gmail.com>
    
    * Added restrictions in accounts api based on the user role
    
    * Added survey name filter in surveys api
    
    * Added pending and overdue count in surveys api
    
    * Added user name
    
    ---------
    
    Signed-off-by: Dominic <dominic097@gmail.com>
    Co-authored-by: Dominic <dominic097@gmail.com>

--- server/controllers/projectController.go
+++ server/controllers/projectController.go
@@ -307,6 +307,7 @@ var UpdateProject = func(w http.ResponseWriter, r *http.Request) {
 		} else {
 			// User exists, update the role
 			user.Role = member.Role
+			user.Name = member.Name
 			if err := db.Save(&user).Error; err != nil {
 				// Handle database error
 				http.Error(w, "Failed to update user", http.StatusInternalServerError)
