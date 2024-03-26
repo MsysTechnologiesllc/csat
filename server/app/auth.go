bfeaac5f7e6de1e50f5f3e4337d4bb82c162d7e3
silambu-msys <107907502+silambu-msys@users.noreply.github.com>
2024-03-26 15:44:19 +0530

    Auth removed for survey details api (#97)

--- server/app/auth.go
+++ server/app/auth.go
@@ -16,7 +16,7 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		notAuth := []string{"/csat/rest/api/user/new", "/csat/rest/api/user/login", "/swagger/"}
+		notAuth := []string{"/csat/rest/api/user/new", "/csat/rest/api/user/login", "/csat/rest/api/survey-details", "/csat/rest/api/manager/survey-details", "/swagger/"}
 		requestPath := r.URL.Path
 
 		// Check if the request is made from Swagger UI
